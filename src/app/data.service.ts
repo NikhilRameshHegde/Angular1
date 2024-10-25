5946b5944d59e74e5a4049857d3e75e9254057e4
NikhilRameshHegde <nikhil36908@gmail.com>
2024-10-25 14:33:26 +0530

    Fourth

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -13,18 +13,8 @@ export class DataService {
     this.products[4] = new Product(432, 'ActFiber', 'Router', 60, 1400);
     this.products[5] = new Product(909, 'Samsung', 'Smart TV', 50, 72500);
     this.products[6] = new Product(654, 'LG', 'Airconditioners', 20, 52500);
-    this.products[7] = new Product(675, 'Crompton',
-      'Ceiling Fans',
-      25,
-      1500
-    );
-    this.products[8] = new Product(
-      2901,
-      'Carrier',
-      'Aircondirioners',
-      5,
-      50500
-    );
+    this.products[7] = new Product(675, 'Crompton', 'Ceiling Fans', 25, 1500);
+    this.products[8] = new Product(2901, 'Carrier', 'Aircondirioners', 5, 50500);
     this.products[9] = new Product(721, 'Usha', 'Table fans', 40, 1200);
     this.products[10] = new Product(454, 'One+', 'Mobiles', 50, 82500);
   }
